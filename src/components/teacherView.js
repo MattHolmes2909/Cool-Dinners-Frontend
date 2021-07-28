fa768c3baa67d427bcca5c49534e8353ef3e10bc
Matt Holmes <matthewholmes95@yahoo.co.uk>
2021-07-28 20:51:23 +0100

    Added axios

--- src/components/teacherView.js
+++ src/components/teacherView.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/TeacherView.css";
 import moment from "moment";
 import pizza from "../images/pizza.png";
@@ -6,6 +6,7 @@ import pasta from "../images/pasta.png";
 import fish from "../images/fish.png";
 import curry from "../images/curry.png";
 import noDinner from "../images/nodinner.png";
+import GetData from "../requests/getData";
 
 const TeacherView = () => {
   let tomorrow;
@@ -17,6 +18,10 @@ const TeacherView = () => {
   } else {
     tomorrow = moment().add(1, "day").endOf("day");
   }
+
+  useEffect(() => {
+    GetData()
+  }, []);
 
   return (
     <div className="TeacherView">
--- src/requests/getData.js
+++ src/requests/getData.js
@@ -2,4 +2,21 @@ import axios from "axios";
 
 const getData = () => {
     
-}
+    let endpoint = "https://cool-dinners.herokuapp.com/child";
+    
+    return axios
+    .get(endpoint)
+    .then((response) => {
+      // handle success
+      console.log(response.data);
+    })
+    .catch((error) => {
+        setAlert({
+            message: "Server error. Please try again later.",
+            isSuccess: false,
+        });
+        console.error("Server error", error);
+    })
+};
+
+export default getData;
